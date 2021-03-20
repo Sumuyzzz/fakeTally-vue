d812dde947ba6fd25287c6828bd6c4d9ef6142eb
sumuyzzz <sumuyzzz@gmail.com>
2021-03-21 04:11:44 +0800

    🎨把各个标签模块化

---
+++ src/components/Types.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Types",
+  data() {
+    return {
+      type: "-",
+    };
+  },
+  methods: {
+    selectType(type) {
+      if (type !== "-" && type !== "+") {
+        throw new Error("type is unknown");
+      }
+      this.type = type;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  display: flex;
+  font-size: 24px;
+  text-align: center;
+  background: #c4c4c4;
+
+  > li {
+    height: 64px;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      height: 4px;
+      width: 100%;
+      background: #333;
+    }
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,105 +1,20 @@
 <template>
   <Layout class-prefix="layout">
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button class="ok">ok</button>
-        <button class="zero">0</button>
-        <button>.</button>
-      </div>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="请输入备注" />
-      </label>
-    </div>
-    <div class="tags">
-      <div class="new">
-        <button>新增标签</button>
-      </div>
-      <ul class="current">
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-        <li>衣</li>
-
-        <li>食</li>
-
-        <li>住</li>
-
-        <li>行</li>
-      </ul>
-    </div>
+    <NumberPad />
+    <Types />
+    <Notes />
+    <Tags />
   </Layout>
 </template>
 
 <script lang="ts">
+import Types from "@/components/Types.vue";
+import Notes from "@/components/Notes.vue";
+import Tags from "@/components/Tags.vue";
+import NumberPad from "@/components/NumberPad.vue";
 export default {
   name: "Money",
+  components: { Types, Notes, Tags, NumberPad },
 };
 </script>
 
