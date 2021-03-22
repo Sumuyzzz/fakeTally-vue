db2a51645e3e3b93810eda1bc3f496c2837dbfc1
sumuyzzz <sumuyzzz@gmail.com>
2021-03-22 09:57:31 +0800

    使用v-model创建双向数据绑定

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -2,14 +2,18 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="请输入备注" />
+      <input type="text" v-model="value" placeholder="请输入备注" />
     </label>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-    name: 'Notes'
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Notes extends Vue {
+  value = "";
 }
 </script>
 
