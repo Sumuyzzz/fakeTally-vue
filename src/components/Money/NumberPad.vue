f09c09aecb1584a7c288bf71b1c535b3d8c57148
Sumuyzzz <Sumuyzzz@gmail.com>
2021-04-11 18:15:48 +0800

    样式不改了

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -92,19 +92,34 @@ export default class NumberPad extends Vue {
       &.zero {
         width: 25% * 2;
       }
-      $bg: #fff;
+      $bg: #f2f2f2;
       &:first-child {
-        background: white;
+        background: $bg;
       }
       &:nth-child(2),
+      &:nth-child(5) {
+        background: darken($bg, 4%);
+      }
       &:nth-child(3),
-      &:nth-child(5),
       &:nth-child(6),
+      &:nth-child(9) {
+        background: darken($bg, 4% * 2);
+      }
+      &:nth-child(4),
       &:nth-child(7),
-      &:nth-child(9),
-      &:nth-child(10),
-      &:nth-child(11) {
-        background: $bg;
+      &:nth-child(10) {
+        background: darken($bg, 4% * 3);
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: darken($bg, 4% * 4);
+      }
+      &:nth-child(14) {
+        background: darken($bg, 4% * 5);
+      }
+      &:nth-child(12) {
+        background: darken($bg, 4% * 6);
       }
     }
   }
