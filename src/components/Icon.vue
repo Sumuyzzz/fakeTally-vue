ab8458e0856ab6dd08efaee3cf72f8f2dabc7dc5
Sumuyzzz <Sumuyzzz@gmail.com>
2021-04-15 14:14:44 +0800

    图标返回功能

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,24 +1,25 @@
 <template>
-<svg class="icon">
-<use :xlink:href="'#'+name"/>
-</svg>
+  <svg class="icon" @click="$emit('click', $event)">
+    <use :xlink:href="'#' + name" />
+  </svg>
 </template>
 
 <script lang="ts">
-  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-  importAll(require.context('../assets/icons/svg', true, /\.svg$/))
-  export default {
-    name: 'Icon',
-    props:[`name`]
-  }
-
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+importAll(require.context("../assets/icons/svg", true, /\.svg$/));
+export default {
+  name: "Icon",
+  props: [`name`],
+};
 </script>
 
 <style lang="scss" scoped>
 .icon {
- width: 1em; height: 1em;
-    vertical-align: -0.15em;
-    fill:currentColor;
-    overflow: hidden;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
