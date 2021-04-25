8274bf715e0b5a42684854275ea478c82059c7ec
Sumuyzzz <Sumuyzzz@gmail.com>
2021-04-25 14:40:16 +0800

    修改了上传金额不为number的bug

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -25,7 +25,7 @@ import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 @Component
 export default class NumberPad extends Vue {
-  @Prop() readonly value!: number;
+  @Prop(Number) readonly value!: number;
   output = this.value.toString();
   //初始值
   inputContent(event: MouseEvent): void {
@@ -58,8 +58,9 @@ export default class NumberPad extends Vue {
     this.output = "0";
   }
   ok() {
-    this.$emit("update:value", this.output);
-    this.$emit("submit", this.output);
+    const number = parseFloat(this.output);
+    this.$emit("update:value", number);
+    this.$emit("submit", number);
     this.output = "0";
   }
 }
