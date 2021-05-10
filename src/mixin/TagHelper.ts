beb12f16a2c3718bf47dab6c764f3ea4fffb04f7
Sumuyzzz <Sumuyzzz@gmail.com>
2021-05-10 16:53:13 +0800

    修改了提交空标签的bug

--- src/mixin/TagHelper.ts
+++ src/mixin/TagHelper.ts
@@ -4,7 +4,7 @@ import Component from 'vue-class-component';
 @Component
 export class TagHelper extends Vue {
     createTag() {
-        const name = window.prompt('请输入标签名');
+        const name = window.prompt('请输入标签名')?.trim();
         if (!name) {
             return window.alert('标签名不能为空');
         }
