1930069601db451acf39432bad026b5c6d9630e6
Sumuyzzz <Sumuyzzz@gmail.com>
2022-05-04 19:16:30 +0800

    更新依赖

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,28 @@
 /* eslint-disable*/
 
-
-
-const path = require("path")
+const path = require('path');
 
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config => {
-    const dir = path.resolve(__dirname, 'src/assets/icons')
+  chainWebpack: (config) => {
+    const dir = path.resolve(__dirname, 'src/assets/icons');
     config.module
       .rule('svg-spirit')
       .test(/\.svg$/)
-      .include.add(dir).end() //只包含icons目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({
-        extract: false
-      }).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{
-      plainSprite: true
-    }])
-    config.module.rule('svg').exclude.add(dir)
-
+      .include.add(dir)
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        extract: false,
+      })
+      .end();
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [
+      {
+        plainSprite: true,
+      },
+    ]);
+    config.module.rule('svg').exclude.add(dir);
   },
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/money_manage/' : '/'
-
-}
+  // publicPath: process.env.NODE_ENV === 'production' ? '/money_manage/' : '/',
+};
