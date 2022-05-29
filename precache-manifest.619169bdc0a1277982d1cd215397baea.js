2145e2f150b8f408cc2d33937f35de9c2c6b0f5e
Sumuyzzz <Sumuyzzz@gmail.com>
2022-05-29 08:23:43 +0800

    deploy

---
+++ precache-manifest.619169bdc0a1277982d1cd215397baea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ca09383e5a8d6eac95",
+    "url": "/fakeTally-vue/css/app.9a09e609.css"
+  },
+  {
+    "revision": "c0d863c8e905ba714db2a7e62dc729e9",
+    "url": "/fakeTally-vue/index.html"
+  },
+  {
+    "revision": "51ca09383e5a8d6eac95",
+    "url": "/fakeTally-vue/js/app.f67d210f.js"
+  },
+  {
+    "revision": "f4b8656fe052dba18082",
+    "url": "/fakeTally-vue/js/chunk-vendors.9c220019.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/fakeTally-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fakeTally-vue/robots.txt"
+  }
+]);
